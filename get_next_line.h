8aaab46caa767689ae7b2837a16e92dcce29bbc7
Wati-Theo42 <schlegel.theo@outlook.fr>
2021-12-14 12:34:51 +0100

    jul

--- get_next_line.h
+++ get_next_line.h
@@ -1,25 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tschlege <tschlege@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/08 16:11:23 by tschlege          #+#    #+#             */
-/*   Updated: 2021/12/08 17:11:02 by tschlege         ###   ########lyon.fr   */
+/*   Created: 2021/12/08 16:09:44 by tschlege          #+#    #+#             */
+/*   Updated: 2021/12/08 17:12:13 by tschlege         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <stdio.h>
+# include <unistd.h> // read
 
-char	*get_next_line(int fd)
-{
-	
-}
+char	*get_next_line(int fd);
 
-int	main(void)
-{
-	printf("%c", get_next_line(2));
-}
+#endif
