0a65db1cc26269b26a021ae0868248e185c9252d
Wati-Theo <schlegel.theo@outlook.fr>
2022-01-04 22:29:06 +0100

    fct where_are_you and ft_strndup created

--- get_next_line.c
+++ get_next_line.c
@@ -3,16 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tschlege <tschlege@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: Wati-Theo <Wati-Theo@protonmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 16:11:23 by tschlege          #+#    #+#             */
-/*   Updated: 2021/12/08 17:11:02 by tschlege         ###   ########lyon.fr   */
+/*   Updated: 2022/01/04 22:28:08 by Wati-Theo        ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 #include <stdio.h>
+
+int	where_are_you(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+char	*ft_strndup(char *str, int size)
+{
+	char	*cpy;
+	int		i;
+
+	cpy = malloc((size + 1) * sizeof(char));
+	if (!cpy)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		cpy[i] = str[i];
+		i++;
+	}
+	cpy[i] = 0;
+	return (cpy);
+}
 
 char	*get_next_line(int fd)
 {
@@ -21,5 +53,12 @@ char	*get_next_line(int fd)
 
 int	main(void)
 {
-	printf("%c", get_next_line(2));
+	// printf("%d\n", where_are_you("oups\nerfzfezf"));
+	// printf("$%s$\n", ft_strndup("oups\n", where_are_you("oups\nerfzfezf")));
+	// printf("%d\n", where_are_you("oup\n"));
+	// printf("$%s$\n", ft_strndup("oups\n", where_are_you("oup\n")));
+	// printf("%d\n", where_are_you("ou\n"));
+	// printf("$%s$\n", ft_strndup("oups\n", where_are_you("ou\n")));
+	// printf("%d\n", where_are_you("oups"));
+	// printf("%d\n", where_are_you(""));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tschlege <tschlege@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: Wati-Theo <Wati-Theo@protonmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 16:09:44 by tschlege          #+#    #+#             */
-/*   Updated: 2021/12/08 17:12:13 by tschlege         ###   ########lyon.fr   */
+/*   Updated: 2022/01/04 20:12:13 by Wati-Theo        ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define GET_NEXT_LINE_H
 
 # include <unistd.h> // read
+# include <stdlib.h> // malloc, free
 
 char	*get_next_line(int fd);
 
