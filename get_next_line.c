73425ab4a0f35bebca3b8a0b5197587af9d47d3f
Wati-Theo42 <schlegel.theo@outlook.fr>
2022-01-06 17:12:33 +0100

    This is a real begining

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Wati-Theo <Wati-Theo@protonmail.com>       +#+  +:+       +#+        */
+/*   By: tschlege <tschlege@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 16:11:23 by tschlege          #+#    #+#             */
-/*   Updated: 2022/01/04 22:28:08 by Wati-Theo        ###   ########lyon.fr   */
+/*   Updated: 2022/01/06 17:11:30 by tschlege         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,19 +46,90 @@ char	*ft_strndup(char *str, int size)
 	return (cpy);
 }
 
+char	*ft_strdup(const char *s1)
+{
+	size_t	s1_len;
+	char	*copy;
+	int		i;
+
+	i = 0;
+	s1_len = ft_strlen(s1);
+	copy = malloc(sizeof(char) * (s1_len + 1));
+	if (!copy)
+		return (NULL);
+	while (s1[i])
+	{
+		copy[i] = s1[i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	stotal_len;
+	char	*the_nouvelle;
+	int		i;
+	int		j;
+
+	if (!s1 && s2)
+		return (ft_strdup(s2));
+	if (!s2 && s1)
+		return (ft_strdup(s1));
+	if (!s1 && !s2)
+		return (NULL);
+	i = 0;
+	j = 0;
+	stotal_len = ft_strlen(s1) + ft_strlen(s2);
+	the_nouvelle = ft_calloc(stotal_len + 1, sizeof(char));
+	if (!the_nouvelle)
+		return (NULL);
+	while (s1[i])
+		the_nouvelle[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		the_nouvelle[j++] = s2[i++];
+	the_nouvelle[j] = '\0';
+	return (the_nouvelle);
+}
+
 char	*get_next_line(int fd)
 {
+	static char	buf[BUFFER_SIZE];
+	char		*first;
+	int			offset;
+	char		*res;
+
 	
+	first = NULL;
+	offset = 0;
+	if (read(fd, buf, BUFFER_SIZE) <= 0) // si read < 1
+		return (NULL);
+	if (where_are_you(buf)) // si '/n' dans buf[BUFFER_SIZE]
+		return (ft_strndup(buf, where_are_you(buf)));
+	else // si pas de '\n'
+	{
+		first = ft_strdup(buf);
+		while (where_are_you(buf) == -1)
+		{
+			if (read(fd, buf, BUFFER_SIZE) <= 0) // si read < 1
+				return (NULL);
+			if (!offset)
+				res = ft_strjoin(first, buf);
+			else
+				res = ft_strjoin(res, buf);
+			offset++;
+		}
+		
+	}
+		
 }
 
 int	main(void)
 {
-	// printf("%d\n", where_are_you("oups\nerfzfezf"));
-	// printf("$%s$\n", ft_strndup("oups\n", where_are_you("oups\nerfzfezf")));
-	// printf("%d\n", where_are_you("oup\n"));
-	// printf("$%s$\n", ft_strndup("oups\n", where_are_you("oup\n")));
-	// printf("%d\n", where_are_you("ou\n"));
-	// printf("$%s$\n", ft_strndup("oups\n", where_are_you("ou\n")));
-	// printf("%d\n", where_are_you("oups"));
-	// printf("%d\n", where_are_you(""));
+	int	fd;
+
+	fd = open("mamamia", O_RDONLY);
+	printf("$%s$\n", get_next_line(fd));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Wati-Theo <Wati-Theo@protonmail.com>       +#+  +:+       +#+        */
+/*   By: tschlege <tschlege@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 16:09:44 by tschlege          #+#    #+#             */
-/*   Updated: 2022/01/04 20:12:13 by Wati-Theo        ###   ########lyon.fr   */
+/*   Updated: 2022/01/05 16:08:42 by tschlege         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <unistd.h> // read
 # include <stdlib.h> // malloc, free
+# include <fcntl.h> // open
 
 char	*get_next_line(int fd);
 
